5c26c5c1fffd177b32466b3627d681781530fc1c
Thomas Woodfin <Twoodfin@berkeley.edu>
2023-11-02 09:27:03 -0600

    Initial commit

---
+++ MVVM_SwiftUI/MVVM_SwiftUIApp.swift
@@ -0,0 +1,16 @@
+//
+//  MVVM_SwiftUIApp.swift
+//  MVVM_SwiftUI
+//
+//  Created by Thomas Woodfin on 10/24/2023.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+        }
+    }
+}
